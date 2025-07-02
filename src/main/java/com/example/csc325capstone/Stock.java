c7834cb2194c3dafc70ec20acf73fb25a9a2a8d5
DanS-89 <stevens.j.daniel@gmail.com>
2025-07-01 21:24:41 -0400

    Create finnhub service class and methods, updated dependencies

--- src/main/java/com/example/csc325capstone/Stock.java
+++ src/main/java/com/example/csc325capstone/Stock.java
@@ -5,13 +5,21 @@ public class Stock {
     private String tickerSymbol;
     private String companyName;
     private double currentPrice;
-    private double percentChange;
+    private double high;
+    private double low;
+    private double open;
+    private double volume;
+    private double previousClose;
 
-    public Stock(String tickerSymbol, String companyName, double currentPrice, double percentChange) {
+    public Stock(String tickerSymbol, String companyName, double currentPrice, double high, double low, double open, double volume, double previousClose) {
         this.tickerSymbol = tickerSymbol;
         this.companyName = companyName;
         this.currentPrice = currentPrice;
-        this.percentChange = percentChange;
+        this.high = currentPrice;
+        this.low = currentPrice;
+        this.open = currentPrice;
+        this.volume = currentPrice;
+        this.previousClose = currentPrice;
     }
 
     public String getTickerSymbol() {
@@ -38,11 +46,43 @@ public class Stock {
         this.currentPrice = currentPrice;
     }
 
-    public double getPercentChange() {
-        return percentChange;
+    public double getHigh() {
+        return high;
     }
 
-    public void setPercentChange(double percentChange) {
-        this.percentChange = percentChange;
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getPreviousClose() {
+        return previousClose;
+    }
+
+    public void setPreviousClose(double previousClose) {
+        this.previousClose = previousClose;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.example.csc325capstone {
     requires javafx.controls;
     requires javafx.fxml;
     requires google.cloud.firestore;
+    requires com.google.gson;
+    requires org.json;
+    requires javax.websocket.api;
 
 
     opens com.example.csc325capstone to javafx.fxml;
