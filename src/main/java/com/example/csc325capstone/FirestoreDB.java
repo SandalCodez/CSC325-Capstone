2735441fcc1fd2efa71291e6b386619a6b77326f
SandalCodez <167233548+SandalCodez@users.noreply.github.com>
2025-07-02 23:20:58 -0400

    connect successful.

--- src/main/java/com/example/csc325capstone/FirestoreDB.java
+++ src/main/java/com/example/csc325capstone/FirestoreDB.java
@@ -1,10 +1,10 @@
 package com.example.csc325capstone;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.cloud.firestore.Firestore;
 import java.io.IOException;
 
 public class FirestoreDB {
@@ -17,6 +17,7 @@ public class FirestoreDB {
             FirebaseApp.initializeApp(options);
             System.out.println("Firebase is initialized");
         } catch (IOException ex) {
+            System.out.println("Firebase is not initialized");
             ex.printStackTrace();
         }
         return FirestoreClient.getFirestore();
